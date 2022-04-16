ea90e4de9d1118d103b6fe5a3c279775787a687f
Renata Vanetta <renatavanetta@outlook.com>
2022-04-16 11:37:24 -0300

    cors config

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const cors = require('cors');
 app.use(express.json());
 app.use(cors({
-    origin: "https://wordle-clone-pug-1-0.herokuapp.com",
+    origin: "https://wordle-clone-pug-1-0.herokuapp.com/",
 }));
 
 const port = process.env.PORT || 3001;
